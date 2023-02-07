c97070638856f4ab8c59d2a9997d0567fb1e39eb
aiotrope <arneli74@protonmail.com>
2023-02-07 22:20:39 +0200

    ♻️ refactor(build.sh): edit file

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ set -o errexit
 
 poetry install
 
-python manage.py makemigrations --dry-run --verbosity 3
-python manage.py makemigrations
-python manage.py migrate
 npm run build
-python manage.py collectstatic --no-input
 
+python manage.py collectstatic --no-input
+python manage.py migrate
